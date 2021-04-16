f8cca4964a3010567ece761031a6036cb39e6599
ahmadms2020 <najyms@gmail.com>
2021-04-16 17:27:36 +0300

    Update UC_customer.xaml.cs

--- POS/POS/View/UC_customer.xaml.cs
+++ POS/POS/View/UC_customer.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace POS.View
 {
@@ -59,7 +60,14 @@ namespace POS.View
             dg_customer.ItemsSource = customers;
         }
 
-        pub void DG_customer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void DG_customer_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Agent agent = new Agent();
             if (dg_customer.SelectedIndex != -1)
@@ -78,7 +86,7 @@ namespace POS.View
         }
 
         private async void  Btn_add_Click(object sender, RoutedEventArgs e)
-        {
+        {//add
             string acc = cb_accType.Text;
 
             if (acc=="Cash")        acc = "c";
@@ -121,6 +129,7 @@ namespace POS.View
             { MainWindow.resourcemanager = new ResourceManager("POS.en_file", Assembly.GetExecutingAssembly()); grid_ucCustomer.FlowDirection = FlowDirection.LeftToRight; }
             else
             { MainWindow.resourcemanager = new ResourceManager("POS.ar_file", Assembly.GetExecutingAssembly()); grid_ucCustomer.FlowDirection = FlowDirection.RightToLeft; }
+
             translate();
 
             //pass parameter type (V for vendors, C for Clients , B for Both)
@@ -130,45 +139,35 @@ namespace POS.View
 
         private void translate()
         {
-           // tb_search.Text = MainWindow.resourcemanager.GetString("trSearchHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_search, MainWindow.resourcemanager.GetString("trSearchHint"));
-            //cb_search.Text = MainWindow.resourcemanager.GetString("trPamentMethodHint");////////////????
             MaterialDesignThemes.Wpf.HintAssist.SetHint(cb_search, MainWindow.resourcemanager.GetString("trPamentMethodHint"));
             txt_baseInformation.Text = MainWindow.resourcemanager.GetString("trBaseInformation");
-            //MaterialDesignThemes.Wpf.HintAssist.SetHint(txt_baseInformation, MainWindow.resourcemanager.GetString("trBaseInformation"));
-            //tb_name.Text = MainWindow.resourcemanager.GetString("trNameHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_name, MainWindow.resourcemanager.GetString("trNameHint"));
-            //tb_address.Text = MainWindow.resourcemanager.GetString("trAdressHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_address, MainWindow.resourcemanager.GetString("trAdressHint"));
-            //tb_company.Text = MainWindow.resourcemanager.GetString("trCompanyHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_company, MainWindow.resourcemanager.GetString("trCompanyHint"));
-            txt_contentInformatin.Text = MainWindow.resourcemanager.GetString("trContactInformation");
-            //MaterialDesignThemes.Wpf.HintAssist.SetHint(txt_contentInformatin, MainWindow.resourcemanager.GetString("trContactInformation"));
-            //cb_area.Text = MainWindow.resourcemanager.GetString("trAreaHint");///////??????????
+            txt_contactInformation.Text = MainWindow.resourcemanager.GetString("trContactInformation");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(cb_area, MainWindow.resourcemanager.GetString("trAreaHint"));
-            //tb_mobile.Text = MainWindow.resourcemanager.GetString("trMobileHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_mobile, MainWindow.resourcemanager.GetString("trMobileHint"));
-            //tb_phone.Text = MainWindow.resourcemanager.GetString("trPhoneHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_phone, MainWindow.resourcemanager.GetString("trPhoneHint"));
-            //tb_email.Text = MainWindow.resourcemanager.GetString("trEmailHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_email, MainWindow.resourcemanager.GetString("trEmailHint"));
             txt_anotherInformation.Text = MainWindow.resourcemanager.GetString("trAnotherInfomation");
-            //MaterialDesignThemes.Wpf.HintAssist.SetHint(txt_anotherInformation, MainWindow.resourcemanager.GetString("trAnotherInfomation"));
-            //tb_details.Text = MainWindow.resourcemanager.GetString("trDetailsHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_details, MainWindow.resourcemanager.GetString("trDetailsHint"));
-            //cb_accType.Text = MainWindow.resourcemanager.GetString("trPamentMethodHint");/////??????
             MaterialDesignThemes.Wpf.HintAssist.SetHint(cb_accType, MainWindow.resourcemanager.GetString("trPamentMethodHint"));
-            //tb_balance.Text = MainWindow.resourcemanager.GetString("trBalanceHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_balance, MainWindow.resourcemanager.GetString("trBalanceHint"));
-            //tb_notes.Text = MainWindow.resourcemanager.GetString("trNoteHint");
             MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_notes, MainWindow.resourcemanager.GetString("trNoteHint"));
             btn_add.Content = MainWindow.resourcemanager.GetString("trAdd");
             btn_update.Content = MainWindow.resourcemanager.GetString("trUpdate");
             btn_delete.Content = MainWindow.resourcemanager.GetString("trDelete");
+            dg_customer.Columns[0].Header = MainWindow.resourcemanager.GetString("trName");
+            dg_customer.Columns[1].Header = MainWindow.resourcemanager.GetString("trCompany");
+            dg_customer.Columns[2].Header = MainWindow.resourcemanager.GetString("trMobile");
+            dg_customer.Columns[3].Header = MainWindow.resourcemanager.GetString("trDetails");
+            dg_customer.Columns[4].Header = MainWindow.resourcemanager.GetString("trBalance");
         }
 
         private async void Btn_update_Click(object sender, RoutedEventArgs e)
-        {
+        {//update
+
             string acc = cb_accType.Text;
 
             if (acc == "Cash") acc = "c";
@@ -177,40 +176,59 @@ namespace POS.View
 
             Agent customer = new Agent
             {
-                agentId= AgentId,
-                name = tb_name.Text,
-                code = "",
-                company = tb_company.Text,
-                address = tb_address.Text,
-                details = tb_details.Text,
-                email = tb_email.Text,
-                phone = tb_phone.Text,
-                mobile = tb_mobile.Text,
-                image = "",
-                type = "c",
-                accType = acc,
-                balance = Single.Parse(tb_balance.Text),
-                isDefault = 0,
-                createDate = DateTime.Now,
-                updateDate = DateTime.Now,
+                agentId      = AgentId,
+                name         = tb_name.Text,
+                code         = "",
+                company      = tb_company.Text,
+                address      = tb_address.Text,
+                details      = tb_details.Text,
+                email        = tb_email.Text,
+                phone        = tb_phone.Text,
+                mobile       = tb_mobile.Text,
+                image        = "",
+                type         = "c",
+                accType      = acc,
+                balance      = Single.Parse(tb_balance.Text),
+                isDefault    = 0,
+                createDate   = DateTime.Now,
+                updateDate   = DateTime.Now,
                 createUserId = 1,
                 updateUserId = 1,
-                notes = tb_notes.Text,
-                isActive = 1,
+                notes        = tb_notes.Text,
+                isActive     = 1,
 
             };
+
             await agentModel.saveAgent(customer);
 
             //pass parameter type (V for vendors, C for Clients , B for Both)
             var agents = await agentModel.GetAgentsAsync("c");
             dg_customer.ItemsSource = agents;
 
+
         }
 
         private async void Btn_delete_Click(object sender, RoutedEventArgs e)
-        {
+        {//delete
             await agentModel.deleteAgent(AgentId);
+
+            //pass parameter type (V for vendors, C for Clients , B for Both)
+            var agents = await agentModel.GetAgentsAsync("c");
+            dg_customer.ItemsSource = agents;
+
+            //clear textBoxs
+            AgentId = 0;
+            tb_name.Clear();
+            tb_company.Clear();
+            tb_address.Clear();
+            tb_details.Clear();
+            tb_email.Clear();
+            tb_phone.Clear();
+            tb_mobile.Clear();
+            cb_accType.SelectedIndex = -1;
+            tb_balance.Clear();
+            tb_notes.Clear();
+
         }
     }
-}
 }
