c78aebd1d484e0025f6c01f88580f8a46854bb55
ahmadms2020 <najyms@gmail.com>
2021-04-22 17:31:12 +0300

    Create Bank.cs

---
+++ POS/POS/Classes/Bank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Classes
+{
+    class Bank
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string mobile { get; set; }
+        public string accNumber { get; set; }
+
+        public DateTime createDate = DateTime.Now;
+        public DateTime updateDate = DateTime.Now;
+        public int createUserId { get; set; }
+        public int updateUserId { get; set; }
+
+        internal Task GetAgentsAsync(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
