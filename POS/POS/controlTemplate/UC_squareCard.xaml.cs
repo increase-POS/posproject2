361239e2d9228fd9d51106e51b822b99076b197f
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2021-04-24 10:55:57 +0300

    Create UC_squareCard.xaml.cs

---
+++ POS/POS/controlTemplate/UC_squareCard.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS.controlTemplate
+{
+    /// <summary>
+    /// Interaction logic for UC_squareCard.xaml
+    /// </summary>
+    public partial class UC_squareCard : UserControl
+    {
+        public UC_squareCard()
+        {
+            InitializeComponent();
+        }
+        public int ContentId { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this;
+        }
+        #region squareCardText
+        public static readonly DependencyProperty squareCardTextDependencyProperty = DependencyProperty.Register("squareCardText",
+            typeof(string),
+            typeof(UC_squareCard),
+            new PropertyMetadata("DEFAULT"));
+        public string squareCardText
+        {
+            set
+            { SetValue(squareCardTextDependencyProperty, value); }
+            get
+            { return (string)GetValue(squareCardTextDependencyProperty); }
+        }
+
+
+        #endregion
+        #region ButtonImageSource
+        public static readonly DependencyProperty squareCardImageSourceDependencyProperty = DependencyProperty.Register("squareCardImageSource",
+            typeof(string),
+            typeof(UC_squareCard),
+            new PropertyMetadata("DEFAULT"));
+        public string squareCardImageSource
+        {
+            set
+            { SetValue(squareCardImageSourceDependencyProperty, value); }
+            get
+            { return (string)GetValue(squareCardImageSourceDependencyProperty); }
+        }
+
+        #endregion
+        #region ButtonBorderBrush
+        public static readonly DependencyProperty squareCardBorderBrushDependencyProperty = DependencyProperty.Register("squareCardBorderBrush",
+            typeof(string),
+            typeof(UC_squareCard),
+            new PropertyMetadata("DEFAULT"));
+        public string squareCardBorderBrush
+        {
+            set
+            { SetValue(squareCardBorderBrushDependencyProperty, value); }
+            get
+            { return (string)GetValue(squareCardBorderBrushDependencyProperty); }
+        }
+        #endregion
+
+       
+    }
+}
