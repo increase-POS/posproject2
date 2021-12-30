95d62807283dff13e96597b04b7c7affb0144736
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2021-12-30 18:46:08 +0200

    wd_taxSetting

---
+++ POS/POS/View/windows/wd_taxSetting.xaml.cs
@@ -0,0 +1,174 @@
+﻿using POS.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_taxSetting.xaml
+    /// </summary>
+    public partial class wd_taxSetting : Window
+    {
+        public wd_taxSetting()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            { SectionData.ExceptionMessage(ex, this); }
+        }
+        BrushConverter bc = new BrushConverter();
+        SetValues setValuesModel = new SetValues();
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+            try
+            {
+                if (sender != null)
+                    SectionData.StartAwait(grid_main);
+
+                #region translate
+
+                if (winLogIn.lang.Equals("en"))
+                {
+                    winLogIn.resourcemanager = new ResourceManager("POS.en_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    winLogIn.resourcemanager = new ResourceManager("POS.ar_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                translate();
+                #endregion
+                if (sender != null)
+                    SectionData.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                if (sender != null)
+                    SectionData.EndAwait(grid_main);
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+
+        private void translate()
+        {
+            txt_title.Text = winLogIn.resourcemanager.GetString("trTax");
+            btn_save.Content = winLogIn.resourcemanager.GetString("trSave");
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                e.Cancel = true;
+                this.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void Tb_validateEmptyLostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = sender.GetType().Name;
+                validateEmpty(name, sender);
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void validateEmpty(string name, object sender)
+        {
+            if (name == "ComboBox")
+            {
+                if ((sender as ComboBox).Name == "cb_firstLevel")
+                    SectionData.validateEmptyComboBox((ComboBox)sender, p_errorFirstLevel, tt_errorFirstLevel, "trFirstPath");
+                else if ((sender as ComboBox).Name == "cb_secondLevel")
+                    SectionData.validateEmptyComboBox((ComboBox)sender, p_errorSecondLevel, tt_errorSecondLevel, "trSecondPath");
+            }
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception ex)
+            {
+                //SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private async void Btn_save_Click(object sender, RoutedEventArgs e)
+        {//save
+            try
+            {
+                if (sender != null)
+                    SectionData.StartAwait(grid_main);
+
+               
+
+                if (sender != null)
+                    SectionData.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                if (sender != null)
+                    SectionData.EndAwait(grid_main);
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+    }
+}
