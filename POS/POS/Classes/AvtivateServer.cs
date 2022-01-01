4dd8b8ae3566102fb176637d70ecc1e25bda82f3
ahmadms2020 <najyms@gmail.com>
2022-01-01 20:06:47 +0200

    prog detail

---
+++ POS/POS/Classes/AvtivateServer.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Security.Claims;
+
+using Newtonsoft.Json.Converters;
+
+namespace POS.Classes
+{
+
+
+    public class AvtivateServer
+    {
+      
+        public string serverUri { get; set; }
+        public string activateCode { get; set; }
+        /// <summary>
+        /// ///////////////////////////////////////
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+
+
+
+
+        public async Task<int> checkconn()
+        {
+            int id = 1;
+            int item = 0;
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("id", id.ToString());
+            //#################
+            IEnumerable<Claim> claims = await APIResult.getList("Activate/checkconn", parameters);
+
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    item = int.Parse(c.Value) ;
+                    break;
+                }
+            }
+            return item;
+
+
+         
+        }
+
+        public async Task<int> Sendserverkey(string skey)
+        {
+            int item = 0;
+           // int res = 0;
+          //  SendDetail item = new SendDetail();
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("skey", skey);
+            //#################
+            IEnumerable<Claim> claims = await APIResult.getList("Activate/Sendserverkey", parameters);
+
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    item = int.Parse(c.Value);
+                  // item = JsonConvert.DeserializeObject<SendDetail>(c.Value, new JsonSerializerSettings { DateParseHandling = DateParseHandling.None });
+
+                    break;
+                }
+            }
+            return item;
+
+        }
+
+        // extend button start:
+        //private async void next_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    int chk = 0;
+        //    string activationkey = "";//get from info 
+        //    try
+        //    {
+        //        if (activationkey.Trim() != "".Trim())
+        //        {
+        //            AvtivateServer ac = new AvtivateServer();
+
+        //            chk = await ac.checkconn();
+
+        //            chk = await ac.Sendserverkey(tb_activationkey.Text);
+
+
+
+        //            if (chk <= 0)
+        //            {
+        //                string message = "inc(" + chk + ")";
+
+        //                string messagecode = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(message));
+        //                tb_activationkey.Text = messagecode;
+
+
+        //                string msg = "The Activation not complete (Error code:" + messagecode + ")";
+
+
+        //                Toaster.ShowWarning(Window.GetWindow(this), message: msg, animation: ToasterAnimation.FadeIn);
+        //            }
+
+        //            else
+        //            {
+        //                Toaster.ShowSuccess(Window.GetWindow(this), message: "The Activation done successfuly", animation: ToasterAnimation.FadeIn);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        Toaster.ShowWarning(Window.GetWindow(this), message: "The server Not Found", animation: ToasterAnimation.FadeIn);
+        //    }
+        //}
+        // extend buttonend
+
+
+
+    }
+}
---
+++ POS/POS/Classes/ProgramDetails.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Security.Claims;
+
+using Newtonsoft.Json.Converters;
+
+namespace POS.Classes
+{
+
+    public class ProgramDetails
+    {
+        public int id { get; set; }
+        public string programName { get; set; }
+        public int branchCount { get; set; }
+        public int posCount { get; set; }
+        public int userCount { get; set; }
+        public int vendorCount { get; set; }
+        public int customerCount { get; set; }
+        public int itemCount { get; set; }
+        public int saleinvCount { get; set; }
+        public Nullable<int> programIncId { get; set; }
+        public Nullable<int> versionIncId { get; set; }
+        public string versionName { get; set; }
+        public int storeCount { get; set; }
+        public string packageSaleCode { get; set; }
+        public string customerServerCode { get; set; }
+        public Nullable<System.DateTime> expireDate { get; set; }
+        public Nullable<bool> isOnlineServer { get; set; }
+        public string packageNumber { get; set; }
+        public Nullable<System.DateTime> updateDate { get; set; }
+        public Nullable<bool> isLimitDate { get; set; }
+        public Nullable<bool> isLimitCount { get; set; }
+        public bool isActive { get; set; }
+        public string packageName { get; set; }
+
+        // current info
+
+        public int branchCountNow { get; set; }
+        public int posCountNow { get; set; }
+        public int userCountNow { get; set; }
+        public int vendorCountNow { get; set; }
+        public int customerCountNow { get; set; }
+        public int itemCountNow { get; set; }
+        public int saleinvCountNow { get; set; }
+
+        public int storeCountNow { get; set; }
+
+        public Nullable<System.DateTime> serverDateNow { get; set; }
+
+
+
+
+        public async Task<ProgramDetails> getCurrentInfo()
+        {
+            ProgramDetails item = new ProgramDetails();
+            IEnumerable<Claim> claims = await APIResult.getList("ProgramDetails/getCurrentInfo");
+            foreach (Claim c in claims)
+            {
+                if (c.Type == "scopes")
+                {
+                    item = JsonConvert.DeserializeObject<ProgramDetails>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" });
+                    break;
+                }
+            }
+            return item;
+        }
+
+
+    }
+}
