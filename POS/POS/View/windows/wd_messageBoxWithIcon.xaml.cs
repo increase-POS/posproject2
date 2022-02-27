6c606f0db9f156590fd28bf58f3c0e3be255b745
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-02-27 14:02:57 +0200

    wd_messageBoxWithIcon

---
+++ POS/POS/View/windows/wd_messageBoxWithIcon.xaml.cs
@@ -0,0 +1,111 @@
+﻿using POS.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_messageBoxWithIcon.xaml
+    /// </summary>
+    public partial class wd_messageBoxWithIcon : Window
+    {
+
+        public bool isOk;
+
+        public wd_messageBoxWithIcon()
+        {
+            try
+            {
+                InitializeComponent();
+                this.DataContext = this;
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                #region translate
+                if (MainWindow.lang.Equals("en"))
+                {
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+                #endregion
+                btn_ok.Content = MainWindow.resourcemanager.GetString("trOK");
+
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                isOk = false;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                SectionData.ExceptionMessage(ex, this);
+            }
+        }
+
+
+        #region contentText
+        public static readonly DependencyProperty contentTextDependencyProperty = DependencyProperty.Register("contentText",
+            typeof(string),
+            typeof(wd_acceptCancelPopup),
+            new PropertyMetadata("DEFAULT"));
+        public string contentText
+        {
+            set
+            { SetValue(contentTextDependencyProperty, value); }
+            get
+            { return (string)GetValue(contentTextDependencyProperty); }
+        }
+        #endregion
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+    }
+}
