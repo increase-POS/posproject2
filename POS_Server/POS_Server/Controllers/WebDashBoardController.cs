12fa20be738959a8cb8d80482e643f97cba7b813
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2022-04-26 14:00:38 +0200

    web dash board

---
+++ POS_Server/POS_Server/Controllers/WebDashBoardController.cs
@@ -0,0 +1,164 @@
+﻿using LinqKit;
+using POS_Server.Models;
+using POS_Server.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace POS_Server.Controllers
+{
+    [RoutePrefix("api/WebDashBoard")]
+    public class WebDashBoardController : ApiController
+    {
+        // GET api/<controller>
+        [HttpPost]
+        [Route("GetDashBoardInfo")]
+        public string GetDashBoardInfo(string token)
+        {
+            token = TokenManager.readToken(HttpContext.Current.Request);
+            var strP = TokenManager.GetPrincipal(token);
+            //if (strP != "0") //invalid authorization
+            //{
+            //    return TokenManager.GenerateToken(strP);
+            //}
+            //else
+            {
+                #region params
+                int branchId = 0;
+                DateTime startDate = DateTime.Now;
+               // DateTime? endDate = null;
+                IEnumerable<Claim> claims = TokenManager.getTokenClaims(token);
+                foreach (Claim c in claims)
+                {
+                    if (c.Type == "branchId")
+                    {
+                        branchId = int.Parse(c.Value);
+                    }
+                    //else if (c.Type == "startDate")
+                    //{
+                    //    startDate = DateTime.Parse(c.Value);
+                    //    startDate = DateTime.Parse(startDate.ToString().Split(' ')[0]);
+
+                    //}
+                    //else if (c.Type == "endDate")
+                    //{
+                    //    if (c.Value != null && c.Value != "")
+                    //    {
+                    //        endDate = DateTime.Parse(c.Value);
+                    //        endDate = DateTime.Parse(endDate.ToString().Split(' ')[0]);
+                    //    }
+                    //}
+                }
+                #endregion
+                //try
+                {
+                    DashBoardModel dashBoardModel = new DashBoardModel();
+                    using (incposdbEntities entity = new incposdbEntities())
+                    {
+                        var searchPredicate = PredicateBuilder.New<invoices>();
+
+                        #region purchases count - vendors
+                        searchPredicate = searchPredicate.And(x => x.isActive == true && (x.invType == "p" || x.invType == "pw"));
+                        if (branchId != 0)
+                            searchPredicate = searchPredicate.And(x => x.branchId == branchId);
+
+                        //if(endDate == null)
+                        //    searchPredicate = searchPredicate.And(x => EntityFunctions.TruncateTime(x.updateDate) == startDate);
+                        //else
+                        //    searchPredicate = searchPredicate.And(x => EntityFunctions.TruncateTime(x.updateDate) >= startDate
+                        //                                            && EntityFunctions.TruncateTime(x.updateDate) <= endDate);
+
+
+                        dashBoardModel.purchasesCount = entity.invoices.Where(searchPredicate).ToList().Count();
+
+                        //vendors count
+                        searchPredicate = searchPredicate.And(x => x.agentId != null);
+                        dashBoardModel.vendorsCount = entity.invoices.Where(searchPredicate).Select(x => x.agentId).ToList().Distinct().Count();
+                       
+
+                        #endregion
+
+                        #region sales count
+                        searchPredicate = PredicateBuilder.New<invoices>();
+
+                        searchPredicate = searchPredicate.And(x => x.isActive == true && x.invType == "s" );
+                        if (branchId != 0)
+                            searchPredicate = searchPredicate.And(x => x.branchId == branchId);
+
+                        //if (endDate == null)
+                        //    searchPredicate = searchPredicate.And(x => EntityFunctions.TruncateTime(x.updateDate) == startDate);
+                        //else
+                        //    searchPredicate = searchPredicate.And(x => EntityFunctions.TruncateTime(x.updateDate) >= startDate
+                        //                                            && EntityFunctions.TruncateTime(x.updateDate) <= endDate);
+
+
+                        dashBoardModel.salesCount = entity.invoices.Where(searchPredicate).Count();
+
+                        //customers count
+                        searchPredicate = searchPredicate.And(x => x.agentId != null);
+                        dashBoardModel.customersCount = entity.invoices.Where(searchPredicate).Select(x => x.agentId).Distinct().Count();
+                        #endregion
+
+                        var posSearchPredicat = PredicateBuilder.New<pos>();
+                        posSearchPredicat = posSearchPredicat.And(x => x.isActive == 1);
+
+                        if (branchId != 0)
+                            posSearchPredicat = posSearchPredicat.And(x => x.branchId == branchId);
+
+                        #region online users
+                        dashBoardModel.onLineUsersCount = (from log in entity.usersLogs
+                                        join p in entity.pos.Where(posSearchPredicat) on log.posId equals p.posId
+                                        join u in entity.users on log.userId equals u.userId
+
+                                        where (log.sOutDate == null && log.users.isOnline == 1)
+
+                                        select new
+                                        {
+                                            log.userId,
+
+                                        }).Distinct().Count();
+
+                        #endregion
+
+                        #region balance
+                        dashBoardModel.balance = (decimal)entity.pos.Where(posSearchPredicat).Select(x => x.balance).Sum();
+                        #endregion
+                        return TokenManager.GenerateToken(dashBoardModel);
+
+                    }
+                }
+                //catch
+                //{
+                //    return TokenManager.GenerateToken("0");
+                //}
+            }
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ POS_Server/POS_Server/Models/WebDashBoardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POS_Server.Models
+{
+    public class WebDashBoardModel
+    {
+        public int purchasesCount { get; set; }
+        public int salesCount { get; set; }
+        public int vendorsCount { get; set; }
+        public int customersCount { get; set; }
+        public int onLineUsersCount { get; set; }
+        public decimal balance { get; set; }
+    }
+}
