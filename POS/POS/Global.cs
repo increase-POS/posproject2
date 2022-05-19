264fdc6c0aca68c5c0319a74ace2dbc0fb0488dc
ahmadms2020 <najyms@gmail.com>
2022-05-19 16:14:12 +0300

    all pos-server

--- POS/POS/Global.cs
+++ POS/POS/Global.cs
@@ -9,8 +9,8 @@ namespace POS
     class Global
     {
         //public const string APIKey = "1234";
-        //public static string APIUri = "http://localhost:44730/api/";
-        public static string APIUri = "http://192.168.1.5:44370/api/";
+        public static string APIUri = "http://localhost:44730/api/";
+        //public static string APIUri = "http://192.168.1.5:44370/api/";
         //public static string APIUri = "http://192.168.1.7:44730/api/";
 
         //public static string APIUri = Properties.Settings.Default.APIUri;
