780321634cc5242f4d52bf2c8c0baae202f931f9
yasmineNjr <82780508+yasmineNjr@users.noreply.github.com>
2022-06-14 12:24:27 +0300

    refresh info after upgrade

--- POS/POS/View/Settings/uc_packageBookSetting.xaml.cs
+++ POS/POS/View/Settings/uc_packageBookSetting.xaml.cs
@@ -60,6 +60,8 @@ namespace POS.View.Settings
             {
                 SectionData.StartAwait(grid_main);
 
+                await RefreshDetailsList();
+
                 #region translate
                 if (MainWindow.lang.Equals("en"))
                 {
@@ -74,50 +76,6 @@ namespace POS.View.Settings
                 translate();
                 #endregion
 
-                progDetails = await progDetailsModel.getCurrentInfo();
-            
-                #region unlimited
-                if (progDetails.branchCount == -1)
-                { dpnl_branch.Visibility = Visibility.Collapsed; txt_branchUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_branch.Visibility = Visibility.Visible; txt_branchUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.storeCount == -1)
-                { dpnl_store.Visibility = Visibility.Collapsed; txt_storeUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_store.Visibility = Visibility.Visible; txt_storeUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.vendorCount == -1)
-                { dpnl_vendor.Visibility = Visibility.Collapsed; txt_vendorUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_vendor.Visibility = Visibility.Visible; txt_vendorUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.userCount == -1)
-                { dpnl_user.Visibility = Visibility.Collapsed; txt_userUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_user.Visibility = Visibility.Visible; txt_userUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.customerCount == -1)
-                { dpnl_customer.Visibility = Visibility.Collapsed; txt_customerUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_customer.Visibility = Visibility.Visible; txt_customerUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.posCount == -1)
-                { dpnl_pos.Visibility = Visibility.Collapsed; txt_posUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_pos.Visibility = Visibility.Visible; txt_posUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.saleinvCount == -1)
-                { dpnl_salesInv.Visibility = Visibility.Collapsed; txt_salesInvUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_salesInv.Visibility = Visibility.Visible; txt_salesInvUnlimited.Visibility = Visibility.Collapsed; }
-
-                if (progDetails.itemCount == -1)
-                { dpnl_item.Visibility = Visibility.Collapsed; txt_itemUnlimited.Visibility = Visibility.Visible; }
-                else
-                { dpnl_item.Visibility = Visibility.Visible; txt_itemUnlimited.Visibility = Visibility.Collapsed; }
-                #endregion
-                
                 if (SectionData.isSupportPermision())
                 {
                     col_upgrade.Width = col_extend.Width;
@@ -127,7 +85,6 @@ namespace POS.View.Settings
                     col_upgrade.Width = new GridLength(0);
                 }
 
-                this.DataContext = progDetails;
 
                 SectionData.EndAwait(grid_main);
             }
@@ -137,6 +94,55 @@ namespace POS.View.Settings
                 SectionData.EndAwait(grid_main);
                 SectionData.ExceptionMessage(ex, this);
             }
+        }
+
+        async Task RefreshDetailsList()
+        {
+            progDetails = await progDetailsModel.getCurrentInfo();
+
+            #region unlimited
+            if (progDetails.branchCount == -1)
+            { dpnl_branch.Visibility = Visibility.Collapsed; txt_branchUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_branch.Visibility = Visibility.Visible; txt_branchUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.storeCount == -1)
+            { dpnl_store.Visibility = Visibility.Collapsed; txt_storeUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_store.Visibility = Visibility.Visible; txt_storeUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.vendorCount == -1)
+            { dpnl_vendor.Visibility = Visibility.Collapsed; txt_vendorUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_vendor.Visibility = Visibility.Visible; txt_vendorUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.userCount == -1)
+            { dpnl_user.Visibility = Visibility.Collapsed; txt_userUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_user.Visibility = Visibility.Visible; txt_userUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.customerCount == -1)
+            { dpnl_customer.Visibility = Visibility.Collapsed; txt_customerUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_customer.Visibility = Visibility.Visible; txt_customerUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.posCount == -1)
+            { dpnl_pos.Visibility = Visibility.Collapsed; txt_posUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_pos.Visibility = Visibility.Visible; txt_posUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.saleinvCount == -1)
+            { dpnl_salesInv.Visibility = Visibility.Collapsed; txt_salesInvUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_salesInv.Visibility = Visibility.Visible; txt_salesInvUnlimited.Visibility = Visibility.Collapsed; }
+
+            if (progDetails.itemCount == -1)
+            { dpnl_item.Visibility = Visibility.Collapsed; txt_itemUnlimited.Visibility = Visibility.Visible; }
+            else
+            { dpnl_item.Visibility = Visibility.Visible; txt_itemUnlimited.Visibility = Visibility.Collapsed; }
+            #endregion
+
+            this.DataContext = progDetails;
         }
         private void translate()
         {
@@ -357,6 +363,8 @@ namespace POS.View.Settings
                 //end activate
 
             }
+
+            await RefreshDetailsList();
         }
 
         private async void Btn_upgrade_Click(object sender, RoutedEventArgs e)
@@ -526,6 +534,7 @@ namespace POS.View.Settings
                
             }
 
+            await RefreshDetailsList();
         }
 
         private async void Btn_serials_Click(object sender, RoutedEventArgs e)
@@ -541,7 +550,7 @@ namespace POS.View.Settings
                 w.ShowDialog();
                 Window.GetWindow(this).Opacity = 1;
 
-                progDetails = await progDetailsModel.getCurrentInfo();
+                await RefreshDetailsList();
 
                 if (sender != null)
                     SectionData.EndAwait(grid_main);
@@ -554,9 +563,5 @@ namespace POS.View.Settings
             }
         }
 
-        private void Btn_extendUpgrade_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
