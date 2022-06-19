5c975e8ee36c58f8bd33730533f6af91737e612b
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-06-19 18:51:20 +0300

    Create decimalToTimeConverter.cs

---
+++ POS/POS/converters/decimalToTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using POS.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace POS.converters
+{
+    class decimalToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return SectionData.decimalToTime((decimal)value);
+            }
+            else
+                return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
